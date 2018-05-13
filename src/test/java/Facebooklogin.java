a03197901f47f8fd2370d031ebd1817403481ba2
IBETEP <33938069+IBETEP@users.noreply.github.com>
2018-05-13 15:52:28 +0300

    Update Facebooklogin.java

--- src/test/java/Facebooklogin.java
+++ src/test/java/Facebooklogin.java
@@ -5,21 +5,16 @@ import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertTrue;
 
 
-public class Facebooklogin {
-
+public class Test {
     @BeforeTest
     public void openBrowser(){
         new BrowserManager().openBrowser();
     }
-
     @Test
-
     public void facebookTest(){
         Navigation.facebookMainPage().facebookLoginPage();
         assertTrue(FacebookLoginPage.getUserName().contains("Igor"));
-
     }
-
     @AfterTest
     public void closeBrowser (){
        new  BrowserManager().closeBrowser();
